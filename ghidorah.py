d2f22ad5fd897fecef43ca3e31e3cb06bcf812ef
George S. <shipmang8@gmail.com>
2025-12-31 02:17:46 -0500

    init

--- ghidorah.py
+++ ghidorah.py
@@ -1,30 +1,80 @@
 
+#1. create a source class
+#2. create classes for each source that inherit from source class
+#3. each class has a search function and a status check function
+#4. for search, just execute each source's search function, same with status check
+#5. return all results in a list
+
+
+
+
+
+class source:
+
+    def __init__(self):
+        print("Initializing...")
+
+    def check_status(url):
+        #ping url for health check
+        #do test queries for information
+        pass
+
+
+class ThePirateBay(source):
+
+    def __init__(self):
+        SELF.url = "Thepiratebay.com"
+
+    def search(self, query):
+        #Check status
+        #parse using beautiful soup
+        #return info back to list
+        pass
+
+
+
+    def check_status(self):
+        Super().check_status(SELF.url)
+
+
+
+
+
+
+def search(query):
+    pass
+
+def statusCheck():
+    pass
+
+
+
+
+
+
+
 
 def main(): 
     while True:
         PrintMenu()
         choice = input("Select an option: ")
         if choice == '1':
-            print("Starting new game...")
-            # Add logic to start a new game
+            choice = input("Enter query:")
+            search(choice)
         elif choice == '2':
-            print("Loading game...")
-            # Add logic to load a game
+            print("Performing status check...")
+
         elif choice == '3':
-            print("Opening options...")
-            # Add logic for options
-        elif choice == '4':
-            print("Exiting game. Goodbye!")
+            print("Exiting...")
             break
         else:
             print("Invalid choice. Please try again.")
 
 
 def PrintMenu():
-    print("1. Start Game")
-    print("2. Load Game")
-    print("3. Options")
-    print("4. Exit")    
+    print("1. Search")
+    print("2. Check Status")
+    print("3. Exit")
 
 
 if __name__ == '__main__':
